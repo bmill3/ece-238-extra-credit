e35ff7b9bbf7fc6e1460c0c3645198e44e09864f
Ben Miller <benmiller@bens-mbp-3.unm.edu>
2025-04-18 17:57:26 -0600

    Refactored main method to account for new page navigation. 'main.dart' acts as a router for the the pages alongside an enterance for web deployments.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'pages/home_page.dart';
+import 'pages/ripple_carry_page.dart';
+import 'pages/gate_level_page.dart';
+import 'pages/delay_propagation_simulator.dart';
+import 'pages/interactive_page.dart';
+
 void main() {
   runApp(const RippleAdderApp());
 }
@@ -10,54 +16,16 @@ class RippleAdderApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ripple-Carry Adder',
+      title: 'Ripple‑Carry Adder',
       debugShowCheckedModeBanner: false,
-      home: const RippleAdderPage(),
-    );
-  }
-}
-
-class RippleAdderPage extends StatelessWidget {
-  const RippleAdderPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(60),
-        child: Container(
-          color: Colors.white,
-          alignment: Alignment.center,
-          child: const Text(
-            '4-Bit Ripple-Carry Adder',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: List.generate(4, (index) {
-            return Container(
-              width: 160, // doubled from 80
-              height: 160, // doubled from 80
-              margin: const EdgeInsets.symmetric(
-                horizontal: 24,
-              ), // doubled from 12
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.grey.shade400, width: 2),
-                // no borderRadius for sharp edges
-              ),
-            );
-          }),
-        ),
-      ),
+      initialRoute: '/',
+      routes: {
+        '/': (c) => const HomePage(),
+        '/ripple': (c) => const RippleCarryPage(),
+        '/gates': (c) => const GateLevelPage(),
+        '/delay': (c) => const DelayPropagationSimulator(),
+        '/interactive': (c) => const InteractivePage(),
+      },
     );
   }
 }
